996e0278f8b3b6ee031ee6b71f80bbaefd923cc9
manuel1710 <javian99anderson@gmail.com>
2019-10-21 15:52:40 -0500

    Exercise 1 completed

---
+++ tic-tac-toe.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+	var board = document.getElementById("board");
+	var boardl= board.length;
+
+	for(var i=0; i<18; i++){
+		document.getElementById("board").childNodes[i].className='square';
+		
+	}
+    
+
+  
+  
+}) 
