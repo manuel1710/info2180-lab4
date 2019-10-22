a161293286bf89297c18705a6adcf41a03a4fc1e
manuel1710 <javian99anderson@gmail.com>
2019-10-21 23:33:43 -0500

    Exercise 2 attempted

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,8 +6,37 @@ document.addEventListener('DOMContentLoaded', function(){
 		document.getElementById("board").childNodes[i].className='square';
 		
 	}
-    
+    var state = [];
+    var players = ["X","O"];
+    var currentTurn = 
 
-  
+    document.getElementsbyClass('square').addEventListener('click',function(){
+    	
+
+    })
+
+    for (var i=0; i<18; i++) {
+    	var j = document.getElementById("board").childNodes[i];
+    	j.addEventListener("click",function(){
+    	    	if(state.length==0){
+	    		j.classList.add('X');
+	    		console.log(j.innerHTML=('X'));
+	    		state.push('X');
+	    	}else{
+	    		if (state[(state.length-1)]=='X'){
+	    			j.innerHTML=('O');
+	    			state.push('O');
+	    		}else{
+	    			j.classList.add('X');
+	    			j.innerHTML=('X');
+	    			state.push('X')
+			}
+		}
+		});
+    };
+
+
+    
+    
   
 }) 
